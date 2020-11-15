f68d8e09d492b36d26ee253224289a00d7fa4bde
Irina Savitskaya <irina_savitskaya1@epam.com>
2020-11-16 01:01:35 +0300

    add creation of signed url

--- import-service/handlers/importProductsFile.js
+++ import-service/handlers/importProductsFile.js
@@ -1,9 +1,24 @@
+const AWS = require('aws-sdk');
+const s3 = new AWS.S3({region: 'eu-central-1'});
+const BUCKET = 'uploaded1';
+
 
 export const importProductsFile = async (event, context, callback) => {
   console.log('Lambda products invocation with event: ', event);
   var fileName = event.queryStringParameters.name;
   console.log('fileName: ', fileName);
-  callback(null, {"statusCode": 200, "body": JSON.stringify("hello")});
+  var key = BUCKET +'/'+ fileName;
+  console.log('key: ', key);
+  const params = {
+    Bucket: BUCKET,
+    Key: key,
+    Expires: 60,
+    ContentType: 'text/csv'
+  };
+
+  var signedUrl = s3.getSignedUrl('putObject', params);
+  callback(null, {"statusCode": 200, "body": signedUrl});
+
   //   try {
   //     const {rows: allProducts} = await client.query(query);
   //     callback(null, {"statusCode": 200, "body": JSON.stringify(allProducts)});
